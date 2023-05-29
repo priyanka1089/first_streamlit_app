a50e6cd47286abe420337f7b137ef9f357a13ead
priyanka1089 <134609702+priyanka1089@users.noreply.github.com>
2023-05-29 12:14:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,5 +36,9 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+
+
 
 
