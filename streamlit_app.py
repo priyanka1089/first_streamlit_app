25b60590ec00afe0a58186d02791ea038269eb5f
priyanka1089 <134609702+priyanka1089@users.noreply.github.com>
2023-05-29 12:15:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ import snowflake.connector
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 
